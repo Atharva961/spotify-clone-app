3ebe2b5eaa894f63947fd20ec5d07ca5b81fb5c8
Atharva961 <atharvaag2002@gmail.com>
2023-04-09 23:34:02 +0530

    Starting Step 4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+    console.log("I HAVE A TOKEN", _token);
+  }, []);
+
   return (
     //BEM 
     <div className="app">
-      <h1>Hello Clever Programmers, We building a spotify clone</h1>
-
-      {/* Spotify Logo */}
-      {/* Login with spotify button */}
-      
+      {
+        token ? (
+          <h1>I am logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
